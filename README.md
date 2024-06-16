8465899f08bca3afaa8f2ce8afd58a007e9f262b
hedin <dima@unest.co>
2024-06-17 00:49:21 +0100

    [feature] Added API support as an simple Flask app without prod supporting

--- README.md
+++ README.md
@@ -7,15 +7,7 @@ The project is ready to run as Docker-based application.
 
 The Makefile is used for convenience with docker-compose. You can do the following by it:
 
-#### Installation
-
-Build application
-```bash
-make build
-```
-
-#### Running
-To run the project (you'll need to open new console to run bash):
+To run the project:
 ```bash
 make run
 ```
@@ -24,12 +16,6 @@ To run the project up as daemon:
 ```bash
 make rund
 ```
-
-To get inside the container:
-```bash
-make bash
-```
-
 
 #### Additional commands
 
@@ -40,15 +26,13 @@ make isort
 
 
 ## Project running
-To start the project, write next command inside terminal after 'make bash' command:
+To use the project, write next cURL inside terminal:
 ```bash
-python main.py
+curl -X POST -H "Content-Type: application/json" -d '{"sentence": "I love programming"}' http://0.0.0.0:8000/predict
 ```
 
 ## Example of project using scenario:
 1) Upload project code from GitHub
 2) Open terminal and write 'cd path_to_app' command
 3) Run 'make rund' command
-4) Run 'make bash' command
-5) Write 'python main.py' command
-6) Wait for a training and write your sentence when "Enter a sentence to predict tag:" question appers
+4) Write ``` curl -X POST -H "Content-Type: application/json" -d '{"sentence": "I love programming"}' http://0.0.0.0:8000/predict ``` command
---
+++ app.py
@@ -0,0 +1,54 @@
+# This is a CodeChallenge's Python project written from assigment1.ipynb notebook
+
+import torch
+from flask import Flask, jsonify, request
+
+from src.data_utils import create_indices, create_tensors, download_data, read_data
+from src.model import BoW
+from src.train import perform_inference, train_bow
+
+app = Flask(__name__)
+
+
+iterations_to_train = 2
+
+# choose device and tensor
+if torch.cuda.is_available():
+    device = 'cuda'
+    tensor_type = torch.cuda.LongTensor
+else:
+    device = 'cpu'
+    tensor_type = torch.LongTensor
+
+# Prepare data to use
+download_data()
+train_data = read_data('data/classes/train.txt')
+test_data = read_data('data/classes/test.txt')
+word_to_index, tag_to_index = create_indices(train_data)
+train_tensors = create_tensors(train_data, word_to_index, tag_to_index)
+test_tensors = create_tensors(test_data, word_to_index, tag_to_index)
+
+# Model parameters
+number_of_words = len(word_to_index)
+number_of_tags = len(tag_to_index)
+model = BoW(number_of_words, number_of_tags, tensor_type).to(device)
+
+# Training and testing of the model
+train_bow(model, train_tensors, test_tensors, device, tensor_type, iterations_to_train)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    sentence = data.get('sentence')
+    if not sentence:
+        return jsonify({'error': 'No sentence provided'}), 400
+    if len(sentence) > 1000:
+        return jsonify({'error': 'Sentence is too big'}), 400
+
+    predicted_tag = perform_inference(model, sentence, word_to_index, tag_to_index, device, tensor_type)
+    return jsonify({'sentence': sentence, 'predicted_tag': predicted_tag})
+
+
+if __name__ == '__main__':
+    app.run()
